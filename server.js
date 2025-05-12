c8f766c75164ac1148dd1d7a369d7008bea48be5
KKA_DESK <Kaanayci@gmail.com>
2025-05-12 15:52:25 +0200

    modification resS setHeader

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "chrome-extension://hochjoliclncpdbbabaebkkflhkpgcdj");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
